a63f84e3fb49834a6b0673007d4c557a2fa5538f
Dwi Rizki <dwirizkikurnia94@gmail.com>
2025-10-08 22:45:32 +0700

    Delete Submission Notes App directory

--- Submission Notes App/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import displayNotes from "./note-list.js";
-import initSearchFeature from "./search.js";
-import initAddNoteForm from "./form.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  displayNotes();
-  initSearchFeature();
-  initAddNoteForm();
-});
